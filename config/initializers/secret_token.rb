6af10203e4a7a48a6103d97569ca158c089c617c
Tomasz Skiba <tomasz.r.skiba@accenture.com>
2011-07-07 18:28:56 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaxterApp2::Application.config.secret_token = '0de5acc2b6da4761ff631d5876fe71b7ee6da2368c3a9e38f87fc3d042f89f018018b03973ffe06c3a982fc88286a9686c5eb7728cce6a83db4c7f052d8b1845'
